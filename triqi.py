40b88867256e4d1908169b72af0d0a287eedcd62
jacobo <jacobopenagos58@gmail.com>
2025-01-24 21:16:15 -0500

    FIN.

--- triqi.py
+++ triqi.py
@@ -1,15 +1,1 @@
-import random
-
-b = [[" "]*3 for _ in range(3)]
-p = ["X", "O"]
-while True:
-    x = random.randint(0, 2)
-    y = random.randint(0, 2)
-    if b[x][y] == " ":
-        b[x][y] = random.choice(p)
-    for r in b:
-        print(r)
-    print("------")
-    if all(c != " " for r in b for c in r):
-        print("Fin del juego")
-        break
+print("Lo borre todo, pero no importa porque igual hay muchas otras versiones en el github.")
